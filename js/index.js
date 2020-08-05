c75f12c55adcb2bdeab3cc4966c168a5a718e472
William Herman <williamajherman@gmail.com>
2020-08-05 12:28:50 -0400

    added mousover for nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// mousover links changes color
+const navLinks = document.querySelectorAll("a");
+console.log(navLinks);
+function turnBlue(event){
+    if (event.target.style.color === "blue"){
+        event.target.style.color = "black";
+    }
+    else{
+        event.target.style.color = "blue";
+    }
+}
+
+navLinks.forEach(link => {link.addEventListener('mouseover', turnBlue)});
+
+// key down makes Fun Bus get big
+
