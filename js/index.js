f80e34235646b4baf47d72ebf9ac564b60caca27
William Herman <williamajherman@gmail.com>
2020-08-05 16:45:18 -0400

    fixed font sizing event

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 
 // mousover links changes color
 const navLinks = document.querySelectorAll("a");
-console.log(navLinks);
 function turnBlue(event){
     if (event.target.style.color === "blue"){
         event.target.style.color = "black";
@@ -14,5 +13,32 @@ function turnBlue(event){
 
 navLinks.forEach(link => {link.addEventListener('mouseover', turnBlue)});
 
-// key down makes Fun Bus get big
+// key down makes Fun Bus get bigger if + is pressed, smaller if -
 
+const funBus = document.querySelector('h1');
+let sizeCounter = 4;
+function fontSizeHandler(){
+    if (event.keyCode === 187){
+        sizeCounter +=.1;
+        funBus.style.fontSize = `${sizeCounter}rem`;
+    }
+    if (event.keyCode === 189){
+        sizeCounter -=.1;
+        funBus.style.fontSize = `${sizeCounter}rem`;
+    }
+    else{
+        return;
+    }
+}
+
+document.addEventListener('keydown', fontSizeHandler);
+
+// add click to navbar
+
+const allImages = document.querySelectorAll('nav');
+allImages.forEach(image => image.addEventListener('click', switchColumn));
+
+function switchColumn(event){
+    event.target.style.display = "flex";
+    event.target.style.flexdirection - "column";
+}
